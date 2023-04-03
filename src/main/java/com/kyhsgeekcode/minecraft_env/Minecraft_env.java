0546e338a1afbe45a60376acd17497a062618b54
KYHSGeekCode <jourhyang123@gmail.com>
2023-04-03 16:58:02 +0900

    :sparkles: Print hello

--- src/main/java/com/kyhsgeekcode/minecraft_env/Minecraft_env.java
+++ src/main/java/com/kyhsgeekcode/minecraft_env/Minecraft_env.java
@@ -1,2 +1,10 @@
-public class Minecraft_env {
+package com.kyhsgeekcode.minecraft_env;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Minecraft_env implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Fabric world!");
+    }
 }
--- src/main/java/com/kyhsgeekcode/minecraft_env/client/Minecraft_envClient.java
+++ src/main/java/com/kyhsgeekcode/minecraft_env/client/Minecraft_envClient.java
@@ -1,2 +1,10 @@
-public class Minecraft_envClient {
+package com.kyhsgeekcode.minecraft_env.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Minecraft_envClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Hello Fabric world! client");
+    }
 }
