88e2412d8c734618f8bc1abfea2cee04b25e6fe8
yhs0602 <jourhyang123@gmail.com>
2024-05-04 17:34:53 +0900

    :sparkles: Check ld_preload

--- src/main/java/com/kyhsgeekcode/minecraft_env/client/Minecraft_envClient.java
+++ src/main/java/com/kyhsgeekcode/minecraft_env/client/Minecraft_envClient.java
@@ -6,5 +6,11 @@ public class Minecraft_envClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         System.out.println("Hello Fabric world! client");
+        var ld_preload = System.getenv("LD_PRELOAD");
+        if (ld_preload != null) {
+            System.out.println("LD_PRELOAD is set: " + ld_preload);
+        } else {
+            System.out.println("LD_PRELOAD is not set");
+        }
     }
 }
