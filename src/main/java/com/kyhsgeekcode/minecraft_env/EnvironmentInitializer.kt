744eec90b31c75d07d93daacd2d691dec8d7d9e0
yhs0602 <jourhyang123@gmail.com>
2024-08-29 16:55:02 +0900

    :zap: Some performance fix

--- src/main/java/com/kyhsgeekcode/minecraft_env/EnvironmentInitializer.kt
+++ src/main/java/com/kyhsgeekcode/minecraft_env/EnvironmentInitializer.kt
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.Element
 import net.minecraft.client.gui.hud.ChatHud
 import net.minecraft.client.gui.screen.TitleScreen
 import net.minecraft.client.gui.screen.world.CreateWorldScreen
+import net.minecraft.client.gui.screen.world.CustomizeFlatLevelScreen
 import net.minecraft.client.gui.screen.world.SelectWorldScreen
 import net.minecraft.client.gui.screen.world.WorldListWidget
 import net.minecraft.client.gui.widget.*
@@ -142,11 +143,6 @@ class EnvironmentInitializer(
                         }
                     }
                 }
-                //                if (widget != null && deleteButton != null) {
-                //                    widget.setSelected(widget.children()[0])
-                //                    deleteButton.onPress()
-                //                    return
-                //                }
                 createButton?.onPress()
             }
 
@@ -221,6 +217,124 @@ class EnvironmentInitializer(
                     }
                 }
                 createButton?.onPress()
+            }
+        }
+    }
+
+    private fun createEmptyWorldAndEnterUsingGUI(client: MinecraftClient) {
+        when (val screen = client.currentScreen) {
+            is TitleScreen -> {
+                screen.children().find {
+                    it is ButtonWidget && it.message.string == "Singleplayer"
+                }?.let {
+                    it as ButtonWidget
+                    it.onPress()
+                    return
+                }
+            }
+
+            is SelectWorldScreen -> {
+                //                println("Select world screen1")
+                var widget: WorldListWidget? = null
+                var deleteButton: ButtonWidget? = null
+                var createButton: ButtonWidget? = null
+                for (child in screen.children()) {
+                    //                    println(child)
+                    if (child is WorldListWidget) {
+                        widget = child
+                    } else if (child is ButtonWidget) {
+                        if (child.message.string == "Delete Selected World") {
+                            deleteButton = child
+                        } else if (child.message.string == "Create New World") {
+                            createButton = child
+                        }
+                    }
+                }
+                createButton?.onPress()
+            }
+
+            is CreateWorldScreen -> {
+                //                println("Create world screen")
+                var createButton: ButtonWidget? = null
+                val cheatRequested = true
+                var indexOfWorldSettingTab = -1
+                var cheatButton: CyclingButtonWidget<*>? = null
+                var settingTabWidget: TabNavigationWidget? = null
+                var worldTypeButton: CyclingButtonWidget<*>? = null
+                var customizeFlatmapButton: ButtonWidget? = null
+                for (child in screen.children()) {
+                    // search for tab navigation widget, to find index of world settings tab
+                    if (indexOfWorldSettingTab == -1 && child is TabNavigationWidget) {
+                        settingTabWidget = child
+                        for (i in child.children().indices) {
+                            val tabChild: Element = child.children()[i]
+                            if (tabChild is TabButtonWidget) {
+                                if (tabChild.message.string == "World") {
+                                    indexOfWorldSettingTab = i
+                                }
+                            }
+                        }
+                    }
+                    // search for create button
+                    if (createButton == null && child is ButtonWidget) {
+                        if (child.message.string == "Create New World") {
+                            createButton = child
+                        }
+                    }
+                    // search for cheat button
+                    if (cheatButton == null && child is CyclingButtonWidget<*>) {
+                        if (child.message.string.startsWith("Allow Commands")) {
+                            cheatButton = child
+                        } else {
+                            println("Cheat button is not found, and the text is ${child.message.string}")
+                        }
+                    }
+                }
+                // Set allow cheats to requested
+                if (cheatButton != null)
+                    setupAllowCheats(cheatButton, cheatRequested)
+                else {
+                    println("Cheat button not found")
+                    throw Exception("Cheat button not found")
+                }
+                // Select world settings tab
+                settingTabWidget!!.selectTab(indexOfWorldSettingTab, false)
+                // Search for seed input
+                if (initialEnvironment.seed != null) {
+                    for (child in screen.children()) {
+                        //                        println(child)
+                        if (child is TextFieldWidget) {
+                            //                            println("Found text field")
+                            child.text = initialEnvironment.seed.toString()
+                        }
+                    }
+                }
+                if (initialEnvironment.isWorldFlat) {
+                    for (child in screen.children()) {
+                        //                        println(child)
+                        if (worldTypeButton == null && child is CyclingButtonWidget<*>
+                            && child.message.string.startsWith("World Type")
+                        ) {
+                            worldTypeButton = child
+                        }
+                        if (customizeFlatmapButton == null && child is ButtonWidget && child.message.string.startsWith("Customize")) {
+                            customizeFlatmapButton = child
+                        }
+                    }
+                    if (worldTypeButton != null) {
+                        while (!worldTypeButton.message.string.endsWith("flat")) {
+                            worldTypeButton.onPress()
+                        }
+                    }
+                    if (customizeFlatmapButton != null) {
+                        customizeFlatmapButton.onPress()
+                    }
+                }
+                createButton?.onPress()
+            }
+
+            is CustomizeFlatLevelScreen -> {
+
             }
         }
     }
