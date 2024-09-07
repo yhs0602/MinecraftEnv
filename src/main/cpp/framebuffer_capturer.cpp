b77ce6cbca69ad28724f2af3099e8db1d073a73c
yhs0602 <jourhyang123@gmail.com>
2024-09-07 21:38:41 +0900

    :necktie: Increase sensitivity

--- src/main/cpp/framebuffer_capturer.cpp
+++ src/main/cpp/framebuffer_capturer.cpp
@@ -114,7 +114,7 @@ enum EncodingMode {
 };
 
 // https://codegolf.stackexchange.com/q/211641/83049
-const GLubyte cursor[28][16] = {
+const GLubyte cursor[24][16] = {
     {2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 
@@ -148,12 +148,7 @@ const GLubyte cursor[28][16] = {
     {0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 2, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 2, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 2, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 2, 0, 0, 0, 0},
-
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 2, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 2, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 2, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0},
 };
 
 extern "C" JNIEXPORT jobject JNICALL Java_com_kyhsgeekcode_minecraft_1env_FramebufferCapturer_captureFramebuffer(
@@ -224,7 +219,7 @@ extern "C" JNIEXPORT jobject JNICALL Java_com_kyhsgeekcode_minecraft_1env_Frameb
         pixels = resizedPixels;
     }
 
-    int cursorHeight = 28;
+    int cursorHeight = 24;
     int cursorWidth = 16;
 
     // 비트맵의 각 픽셀을 원본 이미지에 그리기
--- src/main/java/com/kyhsgeekcode/minecraft_env/Minecraft_env.kt
+++ src/main/java/com/kyhsgeekcode/minecraft_env/Minecraft_env.kt
@@ -500,8 +500,8 @@ class Minecraft_env : ModInitializer, CommandExecutor {
         // TODO: Translate delta camera to mouse movement
 
 //        if (currentScreen != null) {
-        val dy = actionDict.cameraPitch * 3
-        val dx = actionDict.cameraYaw * 3
+        val dy = actionDict.cameraPitch * 4
+        val dx = actionDict.cameraYaw * 4
         MouseInfo.moveMouseBy(dx.toDouble(), dy.toDouble()) // Invert y axis
 //        } else {
 //            // pitch: 0: -90 degree, 24: 90 degree
