fd5beded1581a7e914b1cf3f8a9f109fc35989d9
yhs0602 <jourhyang123@gmail.com>
2024-09-07 16:55:23 +0900

    :loud_sound: Add mouseX y log

--- src/main/java/com/kyhsgeekcode/minecraft_env/mixin/InventoryScreenDrawBackgroundLoggingMixin.java
+++ src/main/java/com/kyhsgeekcode/minecraft_env/mixin/InventoryScreenDrawBackgroundLoggingMixin.java
@@ -2,6 +2,7 @@ package com.kyhsgeekcode.minecraft_env.mixin;
 
 import net.minecraft.client.gui.DrawContext;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -9,8 +10,13 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(net.minecraft.client.gui.screen.ingame.InventoryScreen.class)
 public class InventoryScreenDrawBackgroundLoggingMixin {
+    @Shadow
+    private float mouseX;
+    @Shadow
+    private float mouseY;
+
     @Inject(at = @At("TAIL"), method = "drawBackground", locals = LocalCapture.CAPTURE_FAILSOFT)
     private void drawBackground(DrawContext context, float delta, int mouseX, int mouseY, CallbackInfo ci, int i, int j) {
-        System.out.println("drawBackground: " + context + ", " + delta + ", " + mouseX + ", " + mouseY + "," + i + ", " + j);
+        System.out.println("drawBackground: " + mouseX + "," + mouseY + ", " + delta + ", " + mouseX + ", " + mouseY + "," + i + ", " + j);
     }
 }
